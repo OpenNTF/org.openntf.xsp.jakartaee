b0608777575db2628e1674984952d155bfb42018
Jesse Gallagher <jesse@secondfoundation.org>
2018-06-03 09:44:12 -0400

    Fix request scope invalidation

--- eclipse/plugins/org.openntf.xsp.cdi/src/org/openntf/xsp/cdi/impl/AbstractIdentifiedContext.java
+++ eclipse/plugins/org.openntf.xsp.cdi/src/org/openntf/xsp/cdi/impl/AbstractIdentifiedContext.java
@@ -7,6 +7,7 @@ import org.jboss.weld.contexts.AbstractSharedContext;
 public class AbstractIdentifiedContext extends AbstractSharedContext {
 	private final String id;
 	private final Class<? extends Annotation> scope;
+	private boolean active = true;
 
 	public AbstractIdentifiedContext(String contextId, String id, Class<? extends Annotation> scope) {
 		super(contextId);
@@ -21,6 +22,17 @@ public class AbstractIdentifiedContext extends AbstractSharedContext {
 	
 	public String getId() {
 		return id;
+	}
+	
+	@Override
+	public void invalidate() {
+		super.invalidate();
+		this.active = false;
+	}
+	
+	@Override
+	public boolean isActive() {
+		return active;
 	}
 
 }
