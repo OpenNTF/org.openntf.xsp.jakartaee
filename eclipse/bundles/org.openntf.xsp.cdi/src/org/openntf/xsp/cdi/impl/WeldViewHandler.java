98d6e83588c86b4ec1e89d9a1ff0f90d351c1662
Jesse Gallagher <jesse@secondfoundation.org>
2019-01-22 13:44:08 -0500

    Update license headers

--- eclipse/bundles/org.openntf.xsp.cdi/src/org/openntf/xsp/cdi/impl/WeldViewHandler.java
+++ eclipse/bundles/org.openntf.xsp.cdi/src/org/openntf/xsp/cdi/impl/WeldViewHandler.java
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2018 Jesse Gallagher
+ * Copyright © 2019 Jesse Gallagher
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
