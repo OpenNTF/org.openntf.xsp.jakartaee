fc49de5b46297aee9e0acbe3b7468a1105638bb5
Jesse Gallagher <jesse@secondfoundation.org>
2020-04-13 08:31:54 -0400

    Update license headers

--- eclipse/bundles/org.openntf.xsp.el3/src/org/openntf/xsp/el3/EL3ApplicationListener.java
+++ eclipse/bundles/org.openntf.xsp.el3/src/org/openntf/xsp/el3/EL3ApplicationListener.java
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2020 Jesse Gallagher
+ * Copyright © 2018-2020 Jesse Gallagher
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
