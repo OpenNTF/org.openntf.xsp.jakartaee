19ed984ae51c2ddcd50af24851fce6b65a5c6cab
Jesse Gallagher <jesse@secondfoundation.org>
2020-05-07 08:59:23 -0400

    Remove conversation scope support for now, as its semantics don’t line up quite properly with view scope

--- eclipse/bundles/org.openntf.xsp.cdi/src/org/openntf/xsp/cdi/impl/CDIResolver.java
+++ eclipse/bundles/org.openntf.xsp.cdi/src/org/openntf/xsp/cdi/impl/CDIResolver.java
@@ -18,22 +18,18 @@ package org.openntf.xsp.cdi.impl;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 
-import javax.enterprise.context.ConversationScoped;
 import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.literal.NamedLiteral;
 import javax.enterprise.inject.spi.CDI;
 import javax.faces.context.FacesContext;
 import javax.faces.el.EvaluationException;
 import javax.faces.el.VariableResolver;
 
-import org.jboss.weld.literal.NamedLiteral;
-import org.jboss.weld.manager.BeanManagerImpl;
 import org.openntf.xsp.cdi.CDILibrary;
-import org.openntf.xsp.cdi.context.ConversationViewScopeContext;
 import org.openntf.xsp.cdi.util.ContainerUtil;
 import org.openntf.xsp.jakartaee.LibraryUtil;
 
 import com.ibm.xsp.application.ApplicationEx;
-import com.ibm.xsp.component.UIViewRootEx;
 
 public class CDIResolver extends VariableResolver {
 
@@ -62,17 +58,8 @@ public class CDIResolver extends VariableResolver {
 		if(LibraryUtil.usesLibrary(CDILibrary.LIBRARY_ID, app)) {
 			CDI<Object> container = ContainerUtil.getContainer(app);
 			if(container != null) {
-				// Initialize the view map if it hasn't been already
-				UIViewRootEx viewRoot = (UIViewRootEx)facesContext.getViewRoot();
-				if(viewRoot != null) {
-					BeanManagerImpl manager = ContainerUtil.getBeanManager(app);
-					if(!manager.isContextActive(ConversationScoped.class)) {
-						manager.addContext(new ConversationViewScopeContext(manager.getContextId(), viewRoot.getUniqueViewId()));
-					}
-				}
-				
 				return AccessController.doPrivileged((PrivilegedAction<Object>)() -> {
-					Instance<Object> instance = container.select(new NamedLiteral(name));
+					Instance<Object> instance = container.select(NamedLiteral.of(name));
 					if(instance.isResolvable()) {
 						return instance.get();
 					} else {
