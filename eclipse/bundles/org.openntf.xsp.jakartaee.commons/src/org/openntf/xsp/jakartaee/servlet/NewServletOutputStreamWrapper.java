d8ab0c39ef2d25637e89c44691f938602bf92b7b
Jesse Gallagher <jesse@secondfoundation.org>
2022-02-19 13:30:55 -0500

    Improve delegation in input/output stream wrappers

--- eclipse/bundles/org.openntf.xsp.jakartaee.commons/src/org/openntf/xsp/jakartaee/servlet/NewServletOutputStreamWrapper.java
+++ eclipse/bundles/org.openntf.xsp.jakartaee.commons/src/org/openntf/xsp/jakartaee/servlet/NewServletOutputStreamWrapper.java
@@ -27,7 +27,107 @@ class NewServletOutputStreamWrapper extends javax.servlet.ServletOutputStream {
 	}
 
 	@Override
+	public void flush() throws IOException {
+		delegate.flush();
+	}
+
+	@Override
+	public void close() throws IOException {
+		delegate.close();
+	}
+
+	@Override
+	public void print(boolean b) throws IOException {
+		delegate.print(b);
+	}
+
+	@Override
+	public void print(char c) throws IOException {
+		delegate.print(c);
+	}
+
+	@Override
+	public void print(double d) throws IOException {
+		delegate.print(d);
+	}
+
+	@Override
+	public void print(float f) throws IOException {
+		delegate.print(f);
+	}
+
+	@Override
+	public void print(int i) throws IOException {
+		delegate.print(i);
+	}
+
+	@Override
+	public void print(long l) throws IOException {
+		delegate.print(l);
+	}
+
+	@Override
+	public void print(String arg0) throws IOException {
+		delegate.print(arg0);
+	}
+
+	@Override
+	public void println() throws IOException {
+		delegate.println();
+	}
+
+	@Override
+	public void println(boolean b) throws IOException {
+		delegate.println(b);
+	}
+
+	@Override
+	public void println(char c) throws IOException {
+		delegate.println(c);
+	}
+
+	@Override
+	public void println(double d) throws IOException {
+		delegate.println(d);
+	}
+
+	@Override
+	public void println(float f) throws IOException {
+		delegate.println(f);
+	}
+
+	@Override
+	public void println(int i) throws IOException {
+		delegate.println(i);
+	}
+
+	@Override
+	public void println(long l) throws IOException {
+		delegate.println(l);
+	}
+
+	@Override
+	public void println(String s) throws IOException {
+		delegate.println(s);
+	}
+
+	@Override
 	public void write(int b) throws IOException {
 		delegate.write(b);
+	}
+
+	@Override
+	public void write(byte[] b) throws IOException {
+		delegate.write(b);
+	}
+
+	@Override
+	public void write(byte[] b, int off, int len) throws IOException {
+		delegate.write(b, off, len);
+	}
+
+	@Override
+	public String toString() {
+		return delegate.toString();
 	}
 }
