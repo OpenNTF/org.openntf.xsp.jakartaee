b6d4b8cdd02cd5273f6e5f50a6046c32ed3f4a7f
Jesse Gallagher <jesse@secondfoundation.org>
2022-03-22 11:55:02 -0400

    Add optional CORS configuration for REST (Issue #133)

---
+++ eclipse/tests/it-xsp-jakartaee/src/test/java/it/org/openntf/xsp/jakartaee/nsf/jaxrs/TestCors.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright © 2018-2022 Contributors to the XPages Jakarta EE Support Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package it.org.openntf.xsp.jakartaee.nsf.jaxrs;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import it.org.openntf.xsp.jakartaee.AbstractWebClientTest;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.Response;
+
+@SuppressWarnings("nls")
+public class TestCors extends AbstractWebClientTest {
+	@Test
+	public void testCors() {
+		String expected = "http://someorigin." + System.nanoTime();
+		
+		Client client = getAnonymousClient();
+		WebTarget target = client.target(getRestUrl(null) + "/sample");
+		Response response = target.request()
+			.header("Origin", expected)
+			.get();
+		
+		assertEquals(expected, response.getHeaderString("Access-Control-Allow-Origin"));
+	}
+}
