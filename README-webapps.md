464562f45466ded314829ad97d86d1c93a7241dc
Jesse Gallagher <jesse@secondfoundation.org>
2022-07-25 17:09:04 -0400

    Add JakartaServletFacade for new-to-old Servlet bridging

--- README-webapps.md
+++ README-webapps.md
@@ -1,6 +1,26 @@
 # Use in WebContainer Apps
 
-This project provides a few hooks to ease the use of Jakarta EE technologies in WebContainer webapps - those that use the `com.ibm.pvc.webcontainer.application` extension point and web.xml files. Some pieces - such as Bean Validation - should be usable directly, but others take configuration.
+This project provides a few hooks to ease the use of Jakarta EE technologies in PvC WebContainer webapps - those that use the `com.ibm.pvc.webcontainer.application` extension point and web.xml files. Some pieces - such as Bean Validation - should be usable directly, but others take configuration.
+
+## Servlets
+
+The `org.openntf.xsp.jakarta.servlet` bundle contains a Servlet implementation class `org.openntf.xsp.jakarta.servlet.webapp.JakartaServletFacade`, which can be used to wrap a `jakarta.servlet.Servlet`/`HttpServlet` implementation class for use within Domino's Servlet 2.5 container. This can be done by specifying the `org.openntf.xsp.jakarta.servlet.class` init parameter:
+
+```xml
+<servlet>
+	<servlet-name>ExampleServlet</servlet-name>
+	<servlet-class>org.openntf.xsp.jakarta.servlet.webapp.JakartaServletFacade</servlet-class>
+	
+	<init-param>
+		<param-name>org.openntf.xsp.jakarta.servlet.class</param-name>
+		<param-value>org.openntf.xsp.jakarta.example.webapp.ExampleServlet</param-value>
+	</init-param>
+</servlet>
+<servlet-mapping>
+	<servlet-name>ExampleServlet</servlet-name>
+	<url-pattern>/exampleServlet</url-pattern>
+</servlet-mapping>
+```
 
 ## JSP
 
---
+++ eclipse/bundles/org.openntf.xsp.jakarta.servlet/src/org/openntf/xsp/jakarta/servlet/webapp/JakartaServletFacade.java
@@ -0,0 +1,83 @@
+package org.openntf.xsp.jakarta.servlet.webapp;
+
+import java.io.IOException;
+import java.text.MessageFormat;
+
+import org.openntf.xsp.jakartaee.servlet.ServletUtil;
+
+import com.ibm.commons.util.StringUtil;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * General-purpose Servlet implementation meant to wrap a {@code jakarta.*}
+ * Servlet for a {@link javax.*} environment.
+ * 
+ * <p>Users can specify the Jakarta Servlet class name either by specifying the
+ * {@value #INIT_PARAM_SERVLETCLASS} init parameter or
+ * by subclassing this and overriding {@link #getJakartaServletClassName()}.
+ *  
+ * @author Jesse Gallagher
+ * @since 2.8.0
+ */
+public class JakartaServletFacade extends javax.servlet.http.HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public static final String INIT_PARAM_SERVLETCLASS = "org.openntf.xsp.jakarta.servlet.class"; //$NON-NLS-1$
+	
+	private Servlet delegate;
+
+	@Override
+	public void init(javax.servlet.ServletConfig config) throws javax.servlet.ServletException {
+		super.init(config);
+		
+		String className = getJakartaServletClassName();
+		if(StringUtil.isEmpty(className)) {
+			throw new IllegalArgumentException(MessageFormat.format("Servlet class name must be specified via the {0} init parameter or getJakartaServletClassName()", INIT_PARAM_SERVLETCLASS));
+		}
+		try {
+			@SuppressWarnings("unchecked")
+			Class<? extends Servlet> delegateClass = (Class<? extends Servlet>) Class.forName(className, true, Thread.currentThread().getContextClassLoader());
+			this.delegate = delegateClass.newInstance();
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+			throw new javax.servlet.ServletException(MessageFormat.format("Encountered exception loading Servlet of class {0}", className), e);
+		}
+		
+		try {
+			this.delegate.init(ServletUtil.oldToNew(config));
+		} catch(ServletException e) {
+			throw ServletUtil.newToOld(e);
+		}
+	}
+	
+	@Override
+	protected void service(javax.servlet.http.HttpServletRequest oldReq, javax.servlet.http.HttpServletResponse oldResp)
+			throws javax.servlet.ServletException, IOException {
+		try {
+			HttpServletRequest req = ServletUtil.oldToNew(getServletContext(), oldReq);
+			HttpServletResponse resp = ServletUtil.oldToNew(oldResp);
+			this.delegate.service(req, resp);
+		} catch(ServletException e) {
+			throw ServletUtil.newToOld(e);
+		}
+	}
+	
+	@Override
+	public void destroy() {
+		delegate.destroy();
+		
+		super.destroy();
+	}
+	
+	public String getJakartaServletClassName() {
+		return getServletConfig().getInitParameter(INIT_PARAM_SERVLETCLASS);
+	}
+	
+	public Servlet getDelegate() {
+		return delegate;
+	}
+
+}
