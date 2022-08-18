daa47f91919fdd71a49e67ea1d87cca0f4f68515
Jesse Gallagher <jesse@secondfoundation.org>
2022-08-18 10:06:29 -0400

    Remove unused import

--- eclipse/bundles/org.openntf.xsp.nosql.driver.lsxbe/src/org/openntf/xsp/nosql/communication/driver/lsxbe/impl/DefaultDominoDocumentCollectionManager.java
+++ eclipse/bundles/org.openntf.xsp.nosql.driver.lsxbe/src/org/openntf/xsp/nosql/communication/driver/lsxbe/impl/DefaultDominoDocumentCollectionManager.java
@@ -26,7 +26,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Vector;
-import java.util.function.BiFunction;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
