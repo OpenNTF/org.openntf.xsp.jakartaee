dbce08a3b6d0f8aeca69e25b6ebf6814ef028a79
Jesse Gallagher <jesse@secondfoundation.org>
2023-01-20 11:17:14 -0500

    Add _app tag to metrics with the current app's context path (Issue #69)

---
+++ eclipse/bundles/org.openntf.xsp.microprofile.config/src/org/openntf/xsp/microprofile/config/ext/ImplicitAppConfigProvider.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2018-2023 Contributors to the XPages Jakarta EE Support Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openntf.xsp.microprofile.config.ext;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+import jakarta.annotation.Priority;
+
+/**
+ * This extension class allows for contributors to provide configuration
+ * properties dynamically for an application.
+ * 
+ * <p>These providers can be annotated with {@link Priority} to control
+ * the order in which they are applied. Providers with higher priority
+ * values will be applied after those with lower values.</p> 
+ * 
+ * @author Jesse Gallagher
+ * @since 2.10.0
+ */
+public interface ImplicitAppConfigProvider extends Supplier<Map<String, String>> {
+}
---
+++ eclipse/bundles/org.openntf.xsp.microprofile.metrics/src/org/openntf/xsp/microprofile/metrics/config/MetricsAppConfigSource.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2018-2023 Contributors to the XPages Jakarta EE Support Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openntf.xsp.microprofile.metrics.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openntf.xsp.jakartaee.module.ComponentModuleLocator;
+import org.openntf.xsp.microprofile.config.ext.ImplicitAppConfigProvider;
+
+/**
+ * This MP Config provider produces the current app's context path as a prefix
+ * for metrics, to allow for app-specific metric collection.
+ * 
+ * @author Jesse Gallagher
+ * @since 2.10.0
+ */
+public class MetricsAppConfigSource implements ImplicitAppConfigProvider {
+	public static final String CONFIG_APPNAME = "mp.metrics.appName"; //$NON-NLS-1$
+
+	@Override
+	public Map<String, String> get() {
+		Map<String, String> result = new HashMap<>();
+		ComponentModuleLocator.getDefault()
+			.flatMap(ComponentModuleLocator::getServletContext)
+			.ifPresent(ctx -> {
+				result.put(CONFIG_APPNAME, '/' + ctx.getContextPath());
+			});
+		return result;
+	}
+
+}
