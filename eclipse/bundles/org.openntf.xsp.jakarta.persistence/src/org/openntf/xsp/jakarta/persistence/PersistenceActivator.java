a6f3e53436e220eae54d59af1c9a5bbe1fc31174
Jesse Gallagher <jesse@secondfoundation.org>
2023-03-17 16:05:22 -0400

    Modify Persistence to use JUL logging (Issue #137)

--- eclipse/bundles/org.openntf.xsp.jakarta.persistence/src/org/openntf/xsp/jakarta/persistence/PersistenceActivator.java
+++ eclipse/bundles/org.openntf.xsp.jakarta.persistence/src/org/openntf/xsp/jakarta/persistence/PersistenceActivator.java
@@ -1,5 +1,8 @@
 package org.openntf.xsp.jakarta.persistence;
 
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
@@ -9,6 +12,10 @@ public class PersistenceActivator implements BundleActivator {
 
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {
+		AccessController.doPrivileged((PrivilegedAction<Void>)() -> {
+			System.setProperty("eclipselink.logging.logger", "JavaLogger"); //$NON-NLS-1$ //$NON-NLS-2$
+			return null;
+		});
 		PersistenceProviderResolverHolder.setPersistenceProviderResolver(new EclipseLinkResolver());
 	}
 
