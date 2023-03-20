0a0ef7d63e8f75bed0d0e2b8e40e8b6da7576c42
Jesse Gallagher <jesse@secondfoundation.org>
2023-03-20 14:10:43 -0400

    Add support for REST @Provider classes in the NSF (Issue #382)

---
+++ eclipse/nsfs/nsf-jakartaee-example/odp/Code/Java/rest/ext/ExampleHeaderFilter.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2018-2023 Contributors to the XPages Jakarta EE Support Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package rest.ext;
+
+import java.io.IOException;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+/**
+ * This example REST filter adds a custom outgoing header to all responses.
+ * 
+ * @since 2.11.0
+ */
+@Provider
+public class ExampleHeaderFilter implements ContainerResponseFilter {
+	@Override
+	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+		responseContext.getHeaders().add("X-ExampleHeaderFilter", "hello");
+	}
+}
