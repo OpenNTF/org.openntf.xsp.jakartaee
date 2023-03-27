4524f20c0435b0fa7730230f013d86705ee69364
Jesse Gallagher <jesse@secondfoundation.org>
2023-03-27 11:08:19 -0400

    Add CDI ProxyServices implementation for Java > 8

--- eclipse/bundles/org.openntf.xsp.cdi.jigsaw/src/org/openntf/xsp/cdi/jigsaw/JigsawNSFProxyServices.java
+++ eclipse/bundles/org.openntf.xsp.cdi.jigsaw/src/org/openntf/xsp/cdi/jigsaw/JigsawNSFProxyServices.java
@@ -1,26 +1,11 @@
-/**
- * Copyright (c) 2018-2023 Contributors to the XPages Jakarta EE Support Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.openntf.xsp.cdi.util;
+package org.openntf.xsp.cdi.jigsaw;
 
 import java.security.ProtectionDomain;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.jboss.weld.bean.proxy.util.WeldDefaultProxyServices;
+import org.jboss.weld.exceptions.UnsupportedOperationException;
 
 import com.ibm.domino.xsp.module.nsf.ModuleClassLoader;
 
@@ -31,10 +16,10 @@ import jakarta.annotation.Priority;
  * classes. This is to avoid {@code java.lang.LinkageError: A duplicate class definition for jakarta/enterprise/event/Event$WeldEvent$Proxy$_$$_Weld$Proxy$ is found}
  * 
  * @author Jesse Gallagher
- * @since 2.0.0
+ * @since 2.11.0
  */
-@Priority(0)
-public class NSFProxyServices extends WeldDefaultProxyServices {
+@Priority(1)
+public class JigsawNSFProxyServices extends WeldDefaultProxyServices {
 	
 	private static Map<String, Class<?>> classCache = Collections.synchronizedMap(new HashMap<>());
 	
@@ -66,4 +51,17 @@ public class NSFProxyServices extends WeldDefaultProxyServices {
 		}
 		return super.loadClass(originalClass, classBinaryName);
 	}
+	
+	@Override
+	public boolean supportsClassDefining() {
+		return true;
+	}
+
+	public ClassLoader getClassLoader(final Class<?> proxiedBeanType) {
+        throw new UnsupportedOperationException("WeldDefaultProxyServices.getClassLoader(Class<?>) is not implemented.");
+    }
+
+    public Class<?> loadBeanClass(final String className) {
+        throw new UnsupportedOperationException("WeldDefaultProxyServices.loadBeanClass(String) is not implemented.");
+    }
 }
