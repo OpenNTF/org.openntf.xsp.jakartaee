0a6c07e197a6167dc7e9a5d4db716fd671d19cb8
Jesse Gallagher <jesse@secondfoundation.org>
2023-06-29 12:43:59 -0400

    Remove unused import

--- eclipse/bundles/org.openntf.xsp.cdi/src/org/openntf/xsp/cdi/util/ContainerUtil.java
+++ eclipse/bundles/org.openntf.xsp.cdi/src/org/openntf/xsp/cdi/util/ContainerUtil.java
@@ -24,7 +24,6 @@ import java.text.MessageFormat;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
