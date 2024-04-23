68d3ed26f67ff6972c60df1b93ebf857db483833
Jesse Gallagher <jesse@secondfoundation.org>
2024-04-23 11:39:39 -0400

    Modify MVC test to run twice to demonstrate a bug

--- eclipse/tests/it-xsp-jakartaee/src/test/java/it/org/openntf/xsp/jakartaee/nsf/mvc/TestMvcJsp.java
+++ eclipse/tests/it-xsp-jakartaee/src/test/java/it/org/openntf/xsp/jakartaee/nsf/mvc/TestMvcJsp.java
@@ -18,7 +18,7 @@ package it.org.openntf.xsp.jakartaee.nsf.mvc;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.openqa.selenium.By;
@@ -61,7 +61,9 @@ public class TestMvcJsp extends AbstractWebClientTest {
 		assertEquals("I was sent: Value sent into the tag", dd.getText());
 	}
 	
-	@Test
+	// Account for cases where only the first MVC call works and then "poisons" future ones - ensure that
+	//   a single test can run twice to show it specifically
+	@RepeatedTest(2)
 	public void testBeanParam() {
 		Client client = getAnonymousClient();
 		WebTarget target = client.target(getRestUrl(null, TestDatabase.MAIN) + "/mvc/beanParam"); //$NON-NLS-1$
