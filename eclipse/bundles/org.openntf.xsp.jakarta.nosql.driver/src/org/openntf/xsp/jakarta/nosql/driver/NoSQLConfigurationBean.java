00e54478015025b2fb3cd42259cde677b30f4ae7
Jesse Gallagher <jesse@secondfoundation.org>
2026-06-16 16:55:43 -0400

    Add ability to tell DQL to update views and FT indexes (Issue #755)

--- eclipse/bundles/org.openntf.xsp.jakarta.nosql.driver/src/org/openntf/xsp/jakarta/nosql/driver/NoSQLConfigurationBean.java
+++ eclipse/bundles/org.openntf.xsp.jakarta.nosql.driver/src/org/openntf/xsp/jakarta/nosql/driver/NoSQLConfigurationBean.java
@@ -31,4 +31,32 @@ public interface NoSQLConfigurationBean {
 	 *         explain results during operations
 	 */
 	boolean emitExplainEvents();
+	
+	/**
+	 * Determines whether the DQL engine should refresh
+	 * views for the given entity type and statement
+	 * 
+	 * @param entityType the type of entity being queried
+	 * @param dqlQuery the DQL query to be executed
+	 * @return whether the DQL engine should refresh views
+	 * @since 3.7.0
+	 */
+	default boolean shouldDqlRefreshViews(Class<?> entityType, String dqlQuery) {
+		return false;
+	}
+	
+	/**
+	 * Determines whether the DQL engine should refresh
+	 * the FT index for the given entity type and statement
+	 * 
+	 * @param entityType the type of entity being queried
+	 * @param dqlQuery the DQL query to be executed
+	 * @return whether the DQL engine should refresh the FT index
+	 * @since 3.7.0
+	 */
+	default boolean shouldDqlRefreshFullText(Class<?> entityType, String dqlQuery) {
+		return false;
+	}
+	
+	
 }
